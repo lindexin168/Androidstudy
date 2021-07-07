c29e5895721f3cd1f1025f909206e0a4b2486bd5
lindexin168 <60740169+lindexin168@users.noreply.github.com>
2021-07-07 19:54:20 +0800

    Initial commit

---
+++ app/src/main/java/com/ldx/study/androidstudy/MainActivity.java
@@ -0,0 +1,28 @@
+package com.ldx.study.androidstudy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+//    @Override
+//    protected void onDraw(Canvas canvas) {
+//        super.onDraw(canvas);
+//    }
+//
+//    public MainActivity(Context context) {
+//        super(context);
+//
+//    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
